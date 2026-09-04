d5c2cddf51817c2f5aabb2d3edc4b7137fa9542e
Nikt-Wazny-PL <kapisspam@gmail.com>
2026-09-04 16:27:31 +0200

    sync.h -- concurrent pointer

---
+++ nwlib/src/nwlib/mem/sync.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "ref.h"
+#include <mutex>
+#include <utility>
+
+namespace nw::mem {
+
+template<typename T>
+class sync
+{
+public:
+	sync() = default;
+	sync(const ref<T>& pointer)
+		: m_pointer(pointer) {}
+	sync(const sync<T>&) = default;
+	sync(sync<T>&&) noexcept = default;
+	~sync() = default;
+
+	template<typename TFn>
+	auto access(TFn&& func) -> decltype(std::declval<TFn>()(std::declval<ref<T>&>()))
+	{
+		std::scoped_lock<std::mutex> lock(m_mutex);
+		return func(m_pointer);
+	}
+
+	template<typename... TArgs>
+	static sync<T> create(TArgs&&... args) { return sync<T>(ref<T>::create(std::forward<TArgs>(args)...)); }
+
+	sync<T>& operator=(const sync<T>&) = default;
+	sync<T>& operator=(sync<T>&&) noexcept = default;
+
+private:
+	ref<T>     m_pointer {};
+	std::mutex m_mutex   {};
+};
+
+}
